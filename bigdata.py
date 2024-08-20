0363aceacc11faf42156c25d043142b00f80e84a
arhonah <138059268+arhonah@users.noreply.github.com>
2024-08-20 07:38:12 -0700

    took dash to slash out

--- bigdata.py
+++ bigdata.py
@@ -1,13 +1,19 @@
 from netCDF4 import Dataset
-import os
+import os 
+import datetime
+
+gps_epoch = datetime.datetime(1980, 1, 6)
 f = open("my_bigdata.csv", "w")
-f.write("longitude,latitude,altitude,ttj\n")
+f.write("longitude,latitude,altitude,ttj,critfreq,\n")
 for file in os.listdir("data"):
     if file.endswith(".0001_nc"):
         rootgrp = Dataset("data/"+file, "r")
         lat = rootgrp.edmaxlat
         lon = rootgrp.edmaxlon
         alt = rootgrp.edmaxalt
-        time = rootgrp.edmaxtime
-        f.write(str(lon) + "," + str(lat) + "," + str(alt) + "," + str(time)+"\n")
+        gps_seconds = rootgrp.edmaxtime
+        cf = rootgrp.critfreq
+        datetime_object = gps_epoch + datetime.timedelta(seconds=gps_seconds)
+        dt_str = str(datetime_object)
+        f.write(str(lon) + "," + str(lat) + "," + str(alt) + "," + dt_str + "," + str(cf)+"\n")
 f.close()
